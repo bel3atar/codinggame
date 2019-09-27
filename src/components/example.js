dcf579f454945110fb5945520cac7f3e5eeefb1e
Mohammed Ousama BELATAR <bel3atar@aol.com>
2019-09-27 13:48:57 +0200

    css

--- src/components/example.js
+++ src/components/example.js
@@ -4,16 +4,16 @@ import '@css/components/example.less'
 const VIDEO_TIMEOUT = 1000
 class Example extends Component {
   launchVideo () {
-    console.log('launchVideo called')
-    this.timeout = window.setTimeout(
-      () => Connector.rpc('play', { channelId: 313 }, (a, b) => console.log(a, b)),
+    window.setTimeout(
+      () => Connector.rpc('play', { channelId: 313 }),
       VIDEO_TIMEOUT)
   }
 
   init () {
     this.data = [
       { icon: 'play', text: 'Lire', onTiemout: this.launchVideo.bind(this) },
-      { icon: 'plus', text: 'Ajouter aux favori' }
+      { icon: 'plus', text: 'Ajouter à Ma liste' },
+      { icon: 'thumbs-up', text: 'Évaluer ce titre' }
     ]
   }
 
@@ -21,28 +21,34 @@ class Example extends Component {
     return { data: this.data }
   }
 
-  getList () {
-    const arr = []
-    for (let i = 0, lgt = this.data.length; i < lgt; i++) {
-      arr.push(
-        <div className={['example-element', this.focusableClass]}>{this.data[i].text}</div>
-      )
-    }
-    return arr
-  }
-
   render () {
     return (
       <div className="example">
-        <h1 className="example-title">Star WARS</h1>
-        <i>Use keys Up and Down to select an entry, then use Enter !</i>
+        <h1 className="example-title">Star WARS <i className='fas fa-thumbs-up' /></h1>
+        <img src="https://via.placeholder.com/400x200" />
+        <div className='headband'>
+          <span className='suggest'>Suggéré à 79%</span>
+          <span className='year'>2022</span>
+          <span className='csa'>13+</span>
+          <span className='duration'>1h30min</span>
+          <span classname='quality'>HD</span>
+        </div>
+        <div className='synopsis'>
+          Dans une tour à Paris, deux laveurs de vitres se chamaillent déjouent
+          malgré eux un coup monté pour dérober un puissant tycoon lors d'une
+          fausse prise d'otage.
+        </div>
+        <div className='extra-info'>
+          <span>Distribution: Éric Judor, Marina Foïs, Serge Riaboukine</span>
+          <span>Réalisateur: Charles Nemes</span>
+          <span>Comédies, Comédies françaises</span>
+        </div>
         <ul class='menu'>
           {this.data.map(({ icon, text }) =>
             <li className={`menu-item ${this.focusableClass}`}>
-              <i className={`fas fa-${icon}`}></i>
+              <i className={`fas fa-${icon}`} / >
               <span className='menu-text'>{text}</span>
-            </li>
-          )}
+            </li>)}
         </ul>
       </div>
     )
