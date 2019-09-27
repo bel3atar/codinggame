cdcfe36c212537254fee8304ce62cf5596c86652
Iustin Nita <iustin.nitza@gmail.com>
2019-09-27 15:55:30 +0200

    add video

--- src/components/example.js
+++ src/components/example.js
@@ -1,17 +1,18 @@
 import { Component, h, Router } from 'splay'
-import Connector from '../connector'
 import '@css/components/example.less'
-const VIDEO_TIMEOUT = 1000
+// const VIDEO_TIMEOUT = 1000
 class Example extends Component {
-  launchVideo () {
-    window.setTimeout(
-      () => Connector.rpc('play', { channelId: 313 }),
-      VIDEO_TIMEOUT)
-  }
+  // launchVideo () {
+  //   window.setTimeout(
+  //     () => Connector.rpc('play', { channelId: 313 }),
+  //     VIDEO_TIMEOUT)
+  // }
 
   init () {
     this.data = [
-      { icon: 'play', text: 'Lire', onTiemout: this.launchVideo.bind(this) },
+      { icon: 'play', text: 'Lecture' },
+      { icon: 'th-large', text: 'Titres similaires' },
+      { icon: 'closed-captioning', text: 'Audio et sous-titres' },
       { icon: 'plus', text: 'Ajouter à Ma liste' },
       { icon: 'thumbs-up', text: 'Évaluer ce titre' }
     ]
@@ -24,23 +25,21 @@ class Example extends Component {
   render () {
     return (
       <div className="example">
-        <h1 className="example-title">Star WARS<i className='fas fa-thumbs-up' /></h1>
+        <h1 className="logo"><img src='http://loodibee.com/wp-content/uploads/Star-Wars-transparent-logo.png'/></h1>
         <div className='section-info headband'>
           <span className='suggest'>Suggéré à 79%</span>
-          <span className='year'>2022</span>
+          <span className='year'>2017</span>
           <span className='csa'>13+</span>
-          <span className='duration'>1h30min</span>
+          <span className='duration'>2h32min</span>
           <span className='quality'>HD</span>
         </div>
         <div className='section-info synopsis'>
-          Dans une tour à Paris, deux laveurs de vitres se chamaillent déjouent
-          malgré eux un coup monté pour dérober un puissant tycoon lors d'une
-          fausse prise d'otage.
+        Luke Skywalker's peaceful and solitary existence gets upended when he encounters Rey, a young woman who shows strong signs of the Force. Her desire to learn the ways of the Jedi forces Luke to make a decision that changes their lives forever.
         </div>
         <div className='section-info extra-info'>
-          <p><span className="label">Distribution:</span> Éric Judor, Marina Foïs, Serge Riaboukine</p>
-          <p><span className="label">Réalisateur:</span> Charles Nemes</p>
-          <p>Comédies, Comédies françaises</p>
+          <p><span className="label">Distribution:</span> Daisy Ridley, John Boyega, </p>
+          <p><span className="label">Réalisateur:</span> Rian Johnson</p>
+          <p>Science-Fiction</p>
         </div>
         <ul className='menu'>
           {this.data.map(({ icon, text }) =>
@@ -59,12 +58,17 @@ class Example extends Component {
   mounted () {
     this.focus(this.current)
     this.checkTimeout()
+    this.timeout = setTimeout(() => {
+      const app = document.getElementById('app')
+      app.classList.add('transparent')
+    }, 2000)
   }
 
   checkTimeout () {
-    Connector.rpc('stop')
     const item = this.data[this.current]
     if (item.onTiemout) item.onTiemout()
+    clearTimeout(this.timeout)
+    document.getElementById('app').classList.remove('transparent')
     this.moveSelector(this.current)
   }
   // Keys
@@ -74,6 +78,12 @@ class Example extends Component {
     const next = this.current - 1
     this.focus(next)
     this.checkTimeout()
+    if (next === 0) {
+      this.timeout = setTimeout(() => {
+        const app = document.getElementById('app')
+        app.classList.add('transparent')
+      }, 2000)
+    }
   }
 
   onKeyDown () {
@@ -81,6 +91,7 @@ class Example extends Component {
     const next = this.current + 1
     if (next === max) return
     this.focus(next)
+    clearTimeout(this.timeout)
     this.checkTimeout()
   }
 
